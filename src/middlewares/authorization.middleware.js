3cfaf6cc1a2ab6955e959cde3b76f065581cd621
Tatiana <t.ya14091984@gmail.com>
2026-07-24 21:23:15 +0300

    Add `isAuthor` and `isAuthorOrHasRole` middleware; integrate with forum post endpoints

--- src/middlewares/authorization.middleware.js
+++ src/middlewares/authorization.middleware.js
@@ -1,3 +1,5 @@
+import {getPostById} from "../services/post.service.js";
+
 export const hasRole = role => (req, res, next) => {
     const check = req.principal.roles.includes(role.toUpperCase().trim());
     return check ? next() : res.status(403).json({message: 'Access denied'});
@@ -6,3 +8,15 @@ export const hasRole = role => (req, res, next) => {
 export const isOwner = name => (req, res, next) => req.principal.userName === req.params[name] ? next() : res.status(403).json({message: 'Access denied'});
 
 export const isOwnerOrHasRole = (name, role) => (req, res, next) => req.principal.userName === req.params[name] || req.principal.roles.includes(role.toUpperCase().trim()) ? next() : res.status(403).json({message: 'Access denied'});
+
+export const isAuthor = (postId) => async (req, res, next) => {
+    const post = await getPostById(req.params[postId]);
+    const postAuthor = post.author;
+    return req.principal.userName === postAuthor ? next() : res.status(403).json({message: 'Access denied'});
+}
+
+export const isAuthorOrHasRole = (postId, role) => async (req, res, next) => {
+    const post = await getPostById(req.params[postId]);
+    const postAuthor = post.author;
+    return req.principal.userName === postAuthor || req.principal.roles.includes(role.toUpperCase().trim()) ? next() : res.status(403).json({message: 'Access denied'});
+}
--- src/routes/authorization.routes.js
+++ src/routes/authorization.routes.js
@@ -1,10 +1,20 @@
 import {Router} from "express";
-import {hasRole, isOwner, isOwnerOrHasRole} from "../middlewares/authorization.middleware.js";
-import {ADMIN} from "../configuration/const.js";
+import {
+    hasRole,
+    isAuthor,
+    isAuthorOrHasRole,
+    isOwner,
+    isOwnerOrHasRole
+} from "../middlewares/authorization.middleware.js";
+import {ADMIN, MODERATOR} from "../configuration/const.js";
 
 const router = Router();
 router.all('/account/user/:user/role/:role', hasRole(ADMIN));
 router.patch('/account/user/:user', isOwner('user'));
 router.delete('/account/user/:user', isOwnerOrHasRole('user', ADMIN));
+router.post('/forum/post/:user', isOwner('user'));
+router.patch('/forum/post/:id/comment/:author', isOwner('author'));
+router.patch('/forum/post/:id', isAuthor('id'));
+router.delete('/forum/post/:id', isAuthorOrHasRole('id', MODERATOR));
 
 export default router;
