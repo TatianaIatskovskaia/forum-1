c2c9b859b98b61a57eb3b4e18f4996bcfa5b798e
Tatiana <t.ya14091984@gmail.com>
2026-07-24 19:10:02 +0300

    Add `isOwnerOrHasRole` middleware and integrate it into user deletion endpoint

--- src/middlewares/authorization.middleware.js
+++ src/middlewares/authorization.middleware.js
@@ -5,3 +5,4 @@ export const hasRole = role => (req, res, next) => {
 
 export const isOwner = name => (req, res, next) => req.principal.userName === req.params[name] ? next() : res.status(403).json({message: 'Access denied'});
 
+export const isOwnerOrHasRole = (name, role) => (req, res, next) => req.principal.userName === req.params[name] || req.principal.roles.includes(role.toUpperCase().trim()) ? next() : res.status(403).json({message: 'Access denied'});
--- src/routes/authorization.routes.js
+++ src/routes/authorization.routes.js
@@ -1,9 +1,10 @@
 import {Router} from "express";
-import {hasRole, isOwner} from "../middlewares/authorization.middleware.js";
+import {hasRole, isOwner, isOwnerOrHasRole} from "../middlewares/authorization.middleware.js";
 import {ADMIN} from "../configuration/const.js";
 
 const router = Router();
 router.all('/account/user/:user/role/:role', hasRole(ADMIN));
 router.patch('/account/user/:user', isOwner('user'));
+router.delete('/account/user/:user', isOwnerOrHasRole('user', ADMIN));
 
 export default router;
