78c0f59e49e6597eae8bd81bdb1faa92e1d2f5c8
Tatiana <t.ya14091984@gmail.com>
2026-07-24 17:45:19 +0300

    Refactor role management endpoints to use `all` method for unified handling

--- src/server.js
+++ src/server.js
@@ -15,8 +15,7 @@ app.use(express.json());
 app.use(authentication);
 
 const authorizationRouter = Router();
-authorizationRouter.patch('/account/user/:user/role/:role', hasRole(ADMIN));
-authorizationRouter.delete('/account/user/:user/role/:role', hasRole(ADMIN));
+authorizationRouter.all('/account/user/:user/role/:role', hasRole(ADMIN));
 app.use(authorizationRouter);
 
 app.use('/forum', postRoutes);
